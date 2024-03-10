85f6292a423b4a17e581011a68917675ad7d1496
Kacper Olszanski <kacper.olszanski98@gmail.com>
2024-03-11 00:25:38 +0100

    removed tests from builf

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [dts({ include: ['lib'] })],
+  plugins: [dts({ include: ['lib'], exclude: ['**/*.test.ts'] })],
   build: {
     copyPublicDir: false,
     lib: {
       fileName: 'main',
       name: 'only-utils',
       entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es', 'cjs'],
+      formats: ['es'],
     },
     rollupOptions: {
       external: ['lodash-es', 'url'],
