b749cef7e1d25e6ce25b0a8119215e5c9399e749
Kacper Olszanski <kacper.olszanski@dynatrace.com>
2024-03-22 11:52:53 +0100

    minor improvements

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import dts from 'vite-plugin-dts';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [dts({ include: ['lib'], exclude: ['**/*.test.ts'] })],
+  resolve: {},
   build: {
     lib: {
       fileName: 'main',
@@ -16,7 +17,7 @@ export default defineConfig({
       esmExternals: ['lodash-es'],
     },
     rollupOptions: {
-      external: ['lodash-es', 'url'],
+      external: ['lodash-es', 'lodash', 'url'],
     },
   },
 });
